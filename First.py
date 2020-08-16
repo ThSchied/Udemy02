97f3222356d23f4fa567ba3488ce2b726ba9821f
Thomas Schied <thomas@schied-privat.de>
2020-08-16 09:12:37 +0200

    And & or logic

--- First.py
+++ First.py
@@ -1,1 +1,13 @@
 print("Hello World!")
+
+# and und or logik
+
+print("10 > 12 and 15", 10 > 12 and 15)
+
+print("10 < 12 and 15", 10 < 12 and 15)
+
+print("10 > 12 or 15", 10 > 12 or 15)
+
+print("10 < 12 or 15", 10 < 12 or 15)
+
+
